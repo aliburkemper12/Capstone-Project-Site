fff6eefee2d7fe0128a3a09d9095794d56931ccf
Your Name <you@example.com>
2026-01-28 03:30:53 +0000

    Fixing weird 404 error on refresh

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Deliverables from './pages/Deliverables';
 import Team from './pages/Team';
